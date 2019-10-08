067590e63f3eba65c0c0c5df81a98e0dee606436
Wahyusm <42902872+Wahyusm@users.noreply.github.com>
2019-10-08 07:14:09 +0700

    Add files via upload

---
+++ borocreate.php
@@ -0,0 +1,14 @@
+<?php
+echo "Thanks To : SapaAjah \n\n";
+echo "Mau berapa Acc lo buat? 1\n";
+echo "Save akun di mana gan? : acc1.txt \n";
+echo "[22.33] Cek NIK 3408**********945\n";
+echo "NIK Dapat Digunakan\n";
+echo "Sukses membuat akun\n";
+echo "Jawab Passion\n";
+echo "Wait ea toooodddd\n";
+echo "Selamat anda mendapatkan 10.000 Point\n";
+echo "Save Akun Marlboro :  acc1.txt\n";
+echo "Have AICE day:*\n";
+
+?>
